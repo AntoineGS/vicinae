e1c6982971dd18cf76d4e9f071e2ceb4c30e68ec
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2025-10-27 16:45:37 -0400

    feat(file-search): add support for regex

--- vicinae/src/extensions/file/file-extension.hpp
+++ vicinae/src/extensions/file/file-extension.hpp
@@ -90,7 +90,12 @@ public:
     watcherPaths.setDescription("Semicolon-separated list of paths watched by experimental watcher");
     watcherPaths.setDefaultValue("");
 
-    return {indexing, paths, watcherPaths};
+    auto useRegex = Preference::makeCheckbox("useRegex");
+    useRegex.setTitle("Use regex search");
+    useRegex.setDescription("Enable regular expression matching for file searches");
+    useRegex.setDefaultValue(false);
+
+    return {indexing, paths, watcherPaths, useRegex};
   }
 
   void preferenceValuesChanged(const QJsonObject &preferences) const override {
